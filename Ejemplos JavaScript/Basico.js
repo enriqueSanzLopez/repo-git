e73ff52d3480e6cf904f1780f37c41eacbbc57b1
Enrique Sanz López <enriquesanzlopez@gmail.com>
2022-08-12 23:47:45 +0200

    Recordatorio JavaScript

---
+++ Ejemplos JavaScript/Basico.js
@@ -0,0 +1,18 @@
+//Este es un ejemplo de comentario en línea
+/*Este es un ejemplo de comentario de 
+múltiples líneas*/
+/*Los tipos de datos son undefined, null, boolean, string,
+symbol, number, y object*/
+console.log("Hola mundo");//Esto es el equivalente al print
+var x="B";//Ejemplo de declaración de variable global
+let y="blacaerw";//Ejemplo de declaración de variable local
+const pi=3.14;//Ejemplo de declaración de constante
+y.length;//Longitud de una string
+var ejemplo=[];//Así se declara un array
+ejemplo.push("Hola");//Guarda un valor al final del array ampliando el array
+var nuevoEjemplo=ejemplo.pop();//nuevoEjemplo pasa a ser equivalente al último elemento de ejemplo, y ejemplo pierde su último elemento
+nuevoEjemplo=ejemplo.shift();//nuevoEjemplo pasa a ser equivalente al primer valor del array y ejemplo pierde su primer valor
+ejemplo.unshift("Mundo");//Añade un elemento al principio del array
+function miFuncion(){//Así se declara una función en JavaScript
+    //Instrucciones
+}
