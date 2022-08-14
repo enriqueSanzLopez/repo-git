0878b02a1f65f1ffc17a4e70c68292ee278dbbcf
Enrique Sanz López <enriquesanzlopez@gmail.com>
2022-08-14 19:31:50 +0200

    JavaScript 2

--- Ejemplos JavaScript/Basico.js
+++ Ejemplos JavaScript/Basico.js
@@ -4,15 +4,32 @@ múltiples líneas*/
 /*Los tipos de datos son undefined, null, boolean, string,
 symbol, number, y object*/
 console.log("Hola mundo");//Esto es el equivalente al print
-var x="B";//Ejemplo de declaración de variable global
-let y="blacaerw";//Ejemplo de declaración de variable local
-const pi=3.14;//Ejemplo de declaración de constante
+var x = "B";//Ejemplo de declaración de variable global
+let y = "blacaerw";//Ejemplo de declaración de variable local
+const pi = 3.14;//Ejemplo de declaración de constante
 y.length;//Longitud de una string
-var ejemplo=[];//Así se declara un array
+var ejemplo = [];//Así se declara un array
 ejemplo.push("Hola");//Guarda un valor al final del array ampliando el array
-var nuevoEjemplo=ejemplo.pop();//nuevoEjemplo pasa a ser equivalente al último elemento de ejemplo, y ejemplo pierde su último elemento
-nuevoEjemplo=ejemplo.shift();//nuevoEjemplo pasa a ser equivalente al primer valor del array y ejemplo pierde su primer valor
+var nuevoEjemplo = ejemplo.pop();//nuevoEjemplo pasa a ser equivalente al último elemento de ejemplo, y ejemplo pierde su último elemento
+nuevoEjemplo = ejemplo.shift();//nuevoEjemplo pasa a ser equivalente al primer valor del array y ejemplo pierde su primer valor
 ejemplo.unshift("Mundo");//Añade un elemento al principio del array
-function miFuncion(){//Así se declara una función en JavaScript
+function miFuncion() {//Así se declara una función en JavaScript
     //Instrucciones
+    return 10;
+}
+if (x == "A" && y == "Hola") {//Así se escribe un condicional
+    x = "C";
+} else if (x == "B") {
+    y = "Hola";
+}
+else {
+    x = "B";
+}
+switch (x) {//Así se escribe el switch
+    case "A":
+        break;
+    case "B":
+        break;
+    default:
+        break;
 }
