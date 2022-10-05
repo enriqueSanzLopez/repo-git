a2f1de73277c4824bca0636f65828d9ef0f4c432
Enrique Sanz López <enriquesanzlopez@gmail.com>
2022-10-05 13:35:50 +0200

    Eliminada una carpeta

--- Ejemplos juegos JavaScript/Animar.js
+++
@@ -1,8 +0,0 @@
-/*Guardar los datos del canvas*/
-const canvas=document.getElementById("canvas1");
-const ctx=canvas.getContext("2d");
-const CANVAS_WIDTH=canvas.width=600;
-const CANVAS_HEIGHT=canvas.height=600;
-/*Traer una imagen a JavaScript*/
-const JImagen=new Image();
-Jimagen.src="archivo.png"/*Aquí iría la URL del Spritesheet*/
