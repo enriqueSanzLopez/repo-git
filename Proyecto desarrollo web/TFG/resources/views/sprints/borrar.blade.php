f28362ce6030249066c4312bc45daa067a95294c
Enrique Sanz López <enriquesanzlopez@gmail.com>
2023-05-08 23:03:54 +0200

    Completadas las hojas de lenguajes

--- Proyecto desarrollo web/TFG/resources/views/sprints/borrar.blade.php
+++ Proyecto desarrollo web/TFG/resources/views/sprints/borrar.blade.php
@@ -3,7 +3,7 @@
 @section('contenido')
     <div class="card">
         <div class="card-body">
-            <h5 class="card-title">¿Seguro que quieres eliminar el sprint?</h5>
+            <h5 class="card-title">¿Seguro que quieres eliminar {{ $sprint->name }}?</h5>
             <section>
                 <a href="{{route('sprints.show', $sprint->id)}}" class="btn btn-secondary">Cancelar</a>
                 <a href="{{route('sprints.destroy', $sprint->id)}}" class="btn btn-danger">Eliminar</a>
--- Proyecto desarrollo web/TFG/resources/views/tasks/borrar.blade.php
+++ Proyecto desarrollo web/TFG/resources/views/tasks/borrar.blade.php
@@ -3,7 +3,7 @@
 @section('contenido')
     <div class="card">
         <div class="card-body">
-            <h5 class="card-title">¿Seguro que quieres eliminar la tarea {{$task->name}}?</h5>
+            <h5 class="card-title">¿Seguro que quieres eliminar {{$task->name}}?</h5>
             <section>
                 <a href="{{route('tasks.show', $task->id)}}" class="btn btn-secondary">Cancelar</a>
                 <a href="{{route('tasks.destroy', $task->id)}}" class="btn btn-danger">Eliminar</a>
