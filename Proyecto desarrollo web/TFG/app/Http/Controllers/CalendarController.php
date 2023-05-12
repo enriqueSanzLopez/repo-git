60afeadf8c61aeaa576aceb47e5dc424f804aa50
Enrique Sanz López <enriquesanzlopez@gmail.com>
2023-05-12 23:34:00 +0200

    Creación de calendario sin mostrar

--- Proyecto desarrollo web/TFG/app/Http/Controllers/CalendarController.php
+++ Proyecto desarrollo web/TFG/app/Http/Controllers/CalendarController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Spatie\IcalendarGenerator\Components\Calendar;
+use Spatie\IcalendarGenerator\Components\Event;
 
 class CalendarController extends Controller
 {
@@ -15,6 +16,13 @@ class CalendarController extends Controller
             return redirect('/');
         }
         $calendar=Calendar::create()->name('Calendario de usuario');//Crear el calendario
-        $sprints=Auth::user()->tasks;//Conseguir todos los sprints del usuario
+        $sprints=Auth::user()->sprints;//Conseguir todos los sprints del usuario
+        foreach($sprints as $sprint){//Recorrer los sprints del usuario para crear los eventos del usuario
+            $event=Event::create()->name($sprint->name)->description($sprint->description)->startsAt($sprint->start_date)->endsAt($sprint->limit_date);//Definir un evento por cada sprint
+            foreach($sprint->user as $user){//Se recorre a todos los usuarios del sprint, y se los incluye en el evento
+                $event->attendee($user->email, $user->name);
+            }
+            $calendar->event($event);//Una vez creado el evento, se incluye en el calendario
+        }
     }
 }
