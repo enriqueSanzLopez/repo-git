f9497735bea0d86166e9b06834b6269d65b579ac
Enrique Sanz López <enriquesanzlopez@gmail.com>
2023-05-13 20:14:29 +0200

    Pequeños cambios

--- Proyecto desarrollo web/TFG/public/scripts/show-navigation.js
+++ Proyecto desarrollo web/TFG/public/scripts/show-navigation.js
@@ -4,11 +4,14 @@ let nav_boton=document.getElementById('nav-boton');
 nav_boton.addEventListener('click', function(){
     let navegacion=document.getElementById('navbarNav');
     if(estado==0){
-        navegacion.style.display='flex';
+        navegacion.style.display='block';
         let listado=navegacion.getElementsByTagName('li');
         for(let i=0;i<listado.length;i++){
             listado[i].style.paddingLeft='0';
             listado[i].style.paddingRight='0';
+            listado[i].style.display='flex';
+            listado[i].style.flexDirection='row';
+            listado[i].style.justifyContent='center';
         }
         estado=1;
     }else{
