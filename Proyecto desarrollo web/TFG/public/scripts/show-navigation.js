7b18ee3d29945cf9ce933e7d43b7608766d3522e
Enrique Sanz López <enriquesanzlopez@gmail.com>
2023-05-13 13:32:43 +0200

    Arreglado pequeño problema con la navegación en el responsive por alineado

--- Proyecto desarrollo web/TFG/public/scripts/show-navigation.js
+++ Proyecto desarrollo web/TFG/public/scripts/show-navigation.js
@@ -4,7 +4,9 @@ let nav_boton=document.getElementById('nav-boton');
 nav_boton.addEventListener('click', function(){
     let navegacion=document.getElementById('navbarNav');
     if(estado==0){
-        navegacion.style.display='block';
+        navegacion.style.display='flex';
+        navegacion.style.alignItems='center';
+        navegacion.style.justifyContent='center';
         estado=1;
     }else{
         navegacion.style.display='none';
