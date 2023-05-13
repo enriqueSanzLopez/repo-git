cffb0f87e9e7fc66686adf9f619c960a0151497b
Enrique Sanz López <enriquesanzlopez@gmail.com>
2023-05-13 13:35:04 +0200

    Arreglada alineacion de navegacion en responsive

--- Proyecto desarrollo web/TFG/public/scripts/show-navigation.js
+++ Proyecto desarrollo web/TFG/public/scripts/show-navigation.js
@@ -7,6 +7,11 @@ nav_boton.addEventListener('click', function(){
         navegacion.style.display='flex';
         navegacion.style.alignItems='center';
         navegacion.style.justifyContent='center';
+        let listado=navegacion.getElementsByTagName('li');
+        for(let i=0;i<listado.length;i++){
+            listado[i].style.paddingLeft='0';
+            listado[i].style.paddingRight='0';
+        }
         estado=1;
     }else{
         navegacion.style.display='none';
